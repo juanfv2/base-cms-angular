31b9d9d1ba5cca4cce1a6dfe435be8461489056d
Juan Villalta <juanfv2@gmail.com>
2021-12-30 17:17:33 -0600

    fix: toast

--- projects/base-cms/src/lib/components/toast-container/toast-container.component.ts
+++ projects/base-cms/src/lib/components/toast-container/toast-container.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { JfMessageService } from '../../services/jf-message.service';
+import {Component, TemplateRef} from '@angular/core'
+import {JfMessageService} from '../../services/jf-message.service'
 
 @Component({
   selector: 'base-cms-toast-container',
   templateUrl: './toast-container.component.html',
-  styleUrls: ['./toast-container.component.css']
+  styleUrls: ['./toast-container.component.scss'],
+  host: {'[class.ngb-toasts]': 'true'},
 })
 export class ToastContainerComponent {
-
-  constructor(public messageService: JfMessageService) { }
+  constructor(public messageService: JfMessageService) {}
 
   isTemplate(toast: any) {
     return toast.textOrTpl instanceof TemplateRef
   }
-
 }
