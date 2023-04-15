07add09e2ea8037ed8bc732c047b55fa07c6a175
Juan Villalta <juanfv2@gmail.com>
2023-04-14 20:53:49 -0600

    storage-remove-add when log in/out

--- projects/base-cms/src/lib/support/jf-storage-management.ts
+++ projects/base-cms/src/lib/support/jf-storage-management.ts
@@ -1,72 +1,96 @@
 import {k} from '../environments/k'
 
 export class JfStorageManagement {
+  static kkkEntityGlobalId = `${k.project_name_short}:${k.company}:${k.entityGlobalId}`
+
   static saveCountryInLocalStorage(): any {
     const paths = JfStorageManagement.getPath()
-    console.log('saveCountryInLocalStorage.paths', paths)
-    console.log('saveCountryInLocalStorage.paths', location.pathname)
-    console.log('saveCountryInLocalStorage.paths', location.hash)
 
-    let dev = paths[k.path.dev] === 'dev' ? 'dev' : ''
-    let cName = paths[k.path.company] || 'admin'
-    let entityGlobalId = paths[k.path.country] || 'sv'
+    // console.log('saveCountryInLocalStorage.paths', paths)
+    // console.log('saveCountryInLocalStorage.paths', location.pathname)
+    // console.log('saveCountryInLocalStorage.paths', location.hash)
 
-    localStorage.setItem(`${k.project_name_short}:${k.entityGlobalId}`, entityGlobalId)
-    JfStorageManagement.setItem(k.dev, dev)
-    JfStorageManagement.setItem(k.company_name, cName)
+    const dev = paths[k.path.dev] === 'dev' ? 'dev' : ''
+    const cName = paths[k.path.company] || 'admin'
+    const entityGlobalId = paths[k.path.country] || 'sv'
+
     JfStorageManagement.setItem(k.entityGlobalId, entityGlobalId)
+    JfStorageManagement.setItem(k.company_name, cName)
+    JfStorageManagement.setItem(k.dev, dev)
+
+    k.routes.frontEnd.name = cName
 
     return {dev, entityGlobalId, cName}
   }
 
-  static clearEnvironment(): void {
-    const entityGlobalId = `${JfStorageManagement.getItem(k.entityGlobalId)}`
-    const isDev = `${JfStorageManagement.getItem(k.dev)}`
-    const sideBar = `${JfStorageManagement.getItem(k.isSidebarVisible)}`
-    const cName = `${JfStorageManagement.getItem(k.company_name)}`
-    const c = `${JfStorageManagement.getItem(k.company)}`
+  static clearEnvironment(storage = localStorage): void {
+    const paths = JfStorageManagement.getPath()
+    const cName = paths[k.path.company] || 'admin'
+    const entityGlobalId = paths[k.path.country] || 'sv'
+    const sessionKey = `${k.project_name_short}:${cName}:${entityGlobalId}`
+    const _2save = [
+      k.dev,
+      k.company,
+      k.company_name,
+      k.entityGlobalId,
+      k.isSidebarVisible,
+      k.entityGlobalTimeZone,
+      k.entityGlobalTimeZoneStr,
+    ]
+    const saved = []
 
-    const keys = Object.keys(localStorage)
-    // console.log('localStorage', localStorage)
+    for (let index = 0; index < _2save.length; index++) {
+      const element = `${JfStorageManagement.getItem(_2save[index])}`
+      saved.push(element)
+    }
+
+    const keys = Object.keys(storage)
     let i = keys.length
 
     while (i--) {
       const key = keys[i]
-      if (key.indexOf(cName) > -1) {
-        localStorage.removeItem(key)
+      if (key.indexOf(sessionKey) > -1) {
+        storage.removeItem(key)
       }
     }
 
-    localStorage.setItem(`${k.project_name_short}:${k.entityGlobalId}`, entityGlobalId)
-    JfStorageManagement.setItem(k.entityGlobalId, entityGlobalId)
-    JfStorageManagement.setItem(k.dev, isDev)
-    JfStorageManagement.setItem(k.isSidebarVisible, sideBar)
-    JfStorageManagement.setItem(k.company_name, cName)
-    JfStorageManagement.setItem(k.company, c)
+    for (let index = 0; index < saved.length; index++) {
+      const elementName = _2save[index]
+      const element = saved[index]
+      JfStorageManagement.setItem(elementName, element)
+    }
   }
 
   static currentKey(key: any): string {
     const paths = JfStorageManagement.getPath()
-    let cName = paths[k.path.company] || 'admin'
-    const kkId = localStorage.getItem(`${k.project_name_short}:${k.entityGlobalId}`)
-    const kk = `${k.project_name_short}:${kkId}:${JfStorageManagement.today()}:${key}`
+    const cName = paths[k.path.company] || 'admin'
+    const entityGlobalId = paths[k.path.country] || 'sv'
+    const sessionKey = `${k.project_name_short}:${cName}:${entityGlobalId}`
+    const kk = `${sessionKey}:${JfStorageManagement.today()}:${key}`
     return kk
   }
 
-  static getItem(key: any): string | null {
+  static getItem(key: any, session = false): string | null {
     const kk = this.currentKey(key)
-    const vr = localStorage.getItem(kk)
+    const store = session ? sessionStorage : localStorage
+    const vr = store.getItem(kk)
     return vr
   }
 
-  static setItem(key: any, v: string): void {
+  static setItem(key: any, v: string, session = false): void {
     const kk = this.currentKey(key)
-    return localStorage.setItem(kk, v)
+    const store = session ? sessionStorage : localStorage
+    return store.setItem(kk, v)
   }
 
-  static removeItem(key: string): void {
+  static removeItem(key: string, session = false): void {
     const kk = this.currentKey(key)
-    return localStorage.removeItem(kk)
+    const store = session ? sessionStorage : localStorage
+    return store.removeItem(kk)
+  }
+
+  static today() {
+    return new Date().toJSON().slice(0, 10)
   }
 
   static getPath() {
@@ -74,9 +98,5 @@ export class JfStorageManagement {
     const url = _path || location.hash
     const paths = url.split('/')
     return paths
-  }
-
-  static today() {
-    return new Date().toJSON().slice(0, 10)
   }
 }
