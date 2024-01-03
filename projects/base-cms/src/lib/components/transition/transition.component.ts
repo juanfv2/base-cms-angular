d7c73811c0faed7016651019e48dbabeef29be65
Juan Villalta <juanfv2@gmail.com>
2024-01-03 11:18:56 -0600

    transition

---
+++ projects/base-cms/src/lib/components/transition/transition.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'base-cms-transition',
+  templateUrl: './transition.component.html',
+  styleUrl: './transition.component.css',
+})
+export class TransitionComponent {}
